fa75c516cfce652dc1556c9826cd5812c0e552f8
Mateo Daza <mateodaza@gmail.com>
2019-06-04 22:57:11 -0500

    fast fix

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const withImages = require('next-images')
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 
-module.exports = withImages(withCSS(withSass()));
+// module.exports = withImages(withCSS(withSass()));
+
+module.exports = withImages(withCSS(withSass({
+  target: "serverless",
+  webpack(config, options) {
+    return config
+  }
+})))
